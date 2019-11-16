313fbbf296962af01f7e5ba4fd5c4ab2ee1e0da1
Peter Salin <esalipe@users.noreply.github.com>
2019-11-16 19:25:09 +0000

    prompt.sh: use different prompt as root

--- dotfiles/.config/bash/prompt.sh
+++ dotfiles/.config/bash/prompt.sh
@@ -6,5 +6,9 @@ function parse_git_branch {
 }
 
 MY_TITLEBAR="\[\e]0;\u@\h: \w\$(parse_git_branch)\a\]"
-MY_PROMPT="(\[\e[1;34m\]\h\[\e[0m\])\$(parse_git_branch) \[\e[1m\]\w\[\e[0m\]\n>"
+if [ "$(id -u)" == "0" ]; then
+    MY_PROMPT="(\[\e[1;31m\]\u@\h\[\e[0m\])\$(parse_git_branch) \[\e[1m\]\w\[\e[0m\]\n>"
+else
+    MY_PROMPT="(\[\e[1;34m\]\h\[\e[0m\])\$(parse_git_branch) \[\e[1m\]\w\[\e[0m\]\n>"
+fi
 PS1="$MY_TITLEBAR""$MY_PROMPT"
