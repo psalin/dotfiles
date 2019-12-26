92ff95ee9c00b5bfa63498a883b1325bbd51d0f6
Peter Salin <esalipe@users.noreply.github.com>
2019-12-26 17:27:16 +0200

    bash: use fzf if installed

---
+++ dotfiles/.config/bash/completion.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    # shellcheck source=/dev/null
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    # shellcheck source=/dev/null
+    . /etc/bash_completion
+  fi
+fi
+
+# shellcheck source=/dev/null
+[ -f "${HOME}"/.fzf.bash ] && source "${HOME}"/.fzf.bash
