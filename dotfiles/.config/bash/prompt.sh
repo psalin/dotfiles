af24e40f5ecfe7eab6a8f7b0b571fbf32016d89d
Peter Salin <esalipe@users.noreply.github.com>
2020-06-07 09:09:56 +0300

    scripts,bash: add kube-ps1 for showing k8s info in the bash prompt
    Shows the kubernetes context and namespace in the bash prompt.

--- dotfiles/.config/bash/prompt.sh
+++ dotfiles/.config/bash/prompt.sh
@@ -10,10 +10,20 @@ else
    }
 fi
 
+function kube_ps1 { :; } # Dummy function for when kube-ps1 is not available
+if [[ -x "$(command -v kubectl)" ]]; then
+    install_dir="${HOME}"/.kube-ps1
+
+    if [[ -d "${install_dir}" ]]; then
+        source "${install_dir}"/kube-ps1.sh
+        export KUBE_PS1_SYMBOL_USE_IMG=true
+    fi
+fi
+
 MY_TITLEBAR="\[\e]0;\u@\h: \w\$(__git_ps1)\a\]"
 if [ "$(id -u)" == "0" ]; then
-    MY_PROMPT="(\[\e[1;31m\]\u@\h\[\e[0m\])\$(__git_ps1) \[\e[1m\]\w\[\e[0m\]\n>"
+    MY_PROMPT="(\[\e[1;31m\]\u@\h\[\e[0m\]) \$(kube_ps1)$(__git_ps1) \[\e[1m\]\w\[\e[0m\]\n>"
 else
-    MY_PROMPT="(\[\e[1;34m\]\h\[\e[0m\])\$(__git_ps1) \[\e[1m\]\w\[\e[0m\]\n>"
+    MY_PROMPT="(\[\e[1;34m\]\h\[\e[0m\]) \$(kube_ps1)$(__git_ps1) \[\e[1m\]\w\[\e[0m\]\n>"
 fi
 PS1="$MY_TITLEBAR""$MY_PROMPT"
