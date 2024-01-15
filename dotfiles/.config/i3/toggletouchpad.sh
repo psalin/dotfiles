3bba2fcf3dbf0040981a49d689134271a2f627b5
Peter Salin <21469820+psalin@users.noreply.github.com>
2024-01-15 09:31:07 +0200

    i3: touchpad control changes
    - improve touchpad enable/disable script
    - add alternative touchpad enable/disable key
    - enable middle button clicks by tapping

--- dotfiles/.config/i3/toggletouchpad.sh
+++ dotfiles/.config/i3/toggletouchpad.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-name=$(xinput list --name-only | grep TouchPad)
-id=$(xinput --list --id-only "${name}")
-devEnabled=$(xinput --list-props "${id}" | awk '/Device Enabled/{print !$NF}')
-xinput --set-prop "${id}" 'Device Enabled' "${devEnabled}"
-notify-send --icon computer "${name}" "Device Enabled = ${devEnabled}"
+devices=$(xinput list --id-only)
+for device in ${devices}
+do
+    devpath=$(xinput list-props "${device}" | grep "Device Node" | awk 'NF>1{print $NF}')
+    devpath=${devpath:1:-1}
+    if udevadm info "${devpath}" -q property 2> /dev/null | grep ID_INPUT_TOUCHPAD > /dev/null; then
+        dev_name=$(xinput list --name-only "${device}")
+        dev_enabled=$(xinput --list-props "${device}" | grep "Device Enabled" | awk 'NF>1{print $NF}')
+        if [[ ${dev_enabled} -eq 1 ]]; then
+            xinput disable "${device}"
+            notify-send "${dev_name} (id=${device})" "Device Disabled"
+        else
+            xinput enable "${device}"
+            notify-send "${dev_name} (id=${device})" "Device Enabled"
+        fi
+        break
+    fi
+done
