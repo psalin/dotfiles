265b80558be77c1f04af2662e4b54d8450575a71
Peter Salin <21469820+psalin@users.noreply.github.com>
2025-08-31 18:22:43 +0200

    bash/exports.sh: add comments
    Add explanatory comments on what the exports do

--- dotfiles/.config/bash/03_exports.sh
+++ dotfiles/.config/bash/03_exports.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Use emacs as default editor
 export EDITOR="emacs -nw -q"
-export LESS=MdQiCR
+# Default less options
+# -d suppress dumb terminal error prints
+# -i ignore case when searching
+# -M long-prompt, more info in the status prompt
+# -Q quiet, don't ring the terminal bell
+# -R lets ANSI color escape sequences be interpreted
+export LESS=diMQR
+# Put less history file under XDG
 export LESSHISTFILE="${XDG_STATE_HOME:-$HOME/.local/state}/less/history"
+# Make other-writable directories be listed in colors that can be read
 export LS_COLORS="${LS_COLORS}ow=1;97;45:"
