a72252b38fb6936a259a100f9fcba9d6099dec95
Peter Salin <21469820+psalin@users.noreply.github.com>
2025-11-22 21:42:19 +0100

    bash/fzf: add keyboard shortcuts for fzf searches
    Adds some keyboard shortcuts that can be used in the fzf prompt

--- dotfiles/.config/bash/70_fzf.sh
+++ dotfiles/.config/bash/70_fzf.sh
@@ -2,5 +2,5 @@
 
 if [[ -x "$(command -v fzf)" ]]; then
     eval "$(fzf --bash)"
-    export FZF_DEFAULT_OPTS='--exact'
+    export FZF_DEFAULT_OPTS='--exact --ansi --bind home:first,end:last,ctrl-k:kill-line,alt-d:clear-query'
 fi
