fe3bcee3a7037c6bfed7e56d2af27f6521bf94f3
Peter Salin <21469820+psalin@users.noreply.github.com>
2026-07-06 19:50:27 +0200

    emacs: refactor initialization and settings
    - generalize init.el, general setup and only python lang included
    - enable lazy loading for a quicker startup
    - enable midnight mode for old buffer cleanup
    - remove modes stored under lisp
    - reorganize settings.el to not be in customize format
    - customize can still be used but those settings will be stored in
      custom.el, settings to be stored in this repo should be moved to
      settings.el

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -1,5 +1,4 @@
-;;; init.el --- Initialization file for Emacs
-
+;;; init.el --- Initialization file for Emacs -*- lexical-binding: t -*-
 ;;; Commentary:
 ;; Emacs Startup File --- initialization for Emacs
 
@@ -10,6 +9,12 @@
 (eval-and-compile
   (defun emacs-path (path)
     (expand-file-name path user-emacs-directory)))
+
+;; Git grep in the entire project
+(defun my-helm-git-grep (not-all)
+  (interactive "P")
+  (helm-grep-git-1 default-directory t))
+(global-set-key (kbd "C-c g") 'my-helm-grep-do-git-grep)
 
 ;;; Bootstrap
 
@@ -22,15 +27,6 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
-;; Download packages outside of repositories
-(make-directory (emacs-path "lisp") :parents)
-
-(unless (file-exists-p (emacs-path "lisp/robot-mode.el"))
-  (url-copy-file "https://raw.githubusercontent.com/sakari/robot-mode/master/robot-mode.el" (emacs-path "lisp/robot-mode.el") t))
-
-(unless (file-exists-p (emacs-path "lisp/ttcn3.el"))
-  (url-copy-file "https://raw.githubusercontent.com/dholm/ttcn-el/master/ttcn3.el" (emacs-path "lisp/ttcn3.el") t))
 
 ;;; Environment
 
@@ -51,16 +47,30 @@
 
 ;;; Settings
 
+(setq custom-file "~/.emacs.d/custom.el")
 (eval-and-compile
-  (load (emacs-path "settings")))
+  (load (emacs-path "settings"))
+  (when (file-exists-p (emacs-path "extra_settings.el"))
+    (load (emacs-path "extra_settings")))
+  (when (file-exists-p (emacs-path "custom.el"))
+    (load (emacs-path "custom"))))
 
-(desktop-save-mode t)
-
+(which-key-mode t)
 (global-set-key (kbd "C-x f") #'nil)
 (global-set-key [(shift left)]  'bs-cycle-previous)
 (global-set-key [(shift up)]    'windmove-up)
 (global-set-key [(shift right)] 'bs-cycle-next)
 (global-set-key [(shift down)]  'windmove-down)
+
+;; Use desktop-save-mode but prevent saving until its fully loaded in
+;; order to not lose buffers when quitting before lazy loading is done
+(desktop-save-mode t)
+(require 'desktop)
+(advice-add 'desktop-save :around
+            (lambda (fn &rest args)
+              (if (null desktop-buffer-args-list)
+                  (apply fn args)
+                (message "Buffers still loading, desktop not saved"))))
 
 ;;; Libraries
 (use-package diminish
@@ -82,35 +92,16 @@
   :config
   (avy-setup-default))
 
-(use-package bazel-mode
-  :if (executable-find "bazel")
-  :ensure t
-  :defer t
-  :mode "BUILD\\'"
-  :hook (bazel-mode . (lambda() (when (executable-find "buildifier")
-                                  (add-hook 'before-save-hook 'bazel-format nil t)))))
-
-(use-package cc-mode
-  :defer t
-  :hook (c-mode-common . (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace))))
-
 (use-package company
   :ensure t
   :defer t
-  :hook ((c++-mode . company-mode)
-         (c-mode . company-mode)))
+  :hook ((python-mode . company-mode)))
 
 (use-package company-anaconda
   :ensure t
   :after (anaconda-mode company)
   :init
   (add-to-list 'company-backends 'company-anaconda))
-
-(use-package company-irony
-  :ensure t
-  :after (irony company)
-  :init
-  (add-to-list 'company-backends 'company-irony))
 
 (use-package diff-hl
   :ensure t
@@ -120,33 +111,10 @@
   (global-diff-hl-mode t)
   (unless (display-graphic-p) (diff-hl-margin-mode)))
 
-(use-package irony
-  :ensure t
-  :config
-  (defun my-irony-mode-on ()
-  ;; avoid enabling irony-mode in modes that inherits c-mode, e.g: php-mode
-  (when (member major-mode irony-supported-major-modes)
-    (irony-mode 1)))
-  :hook ((c++-mode . my-irony-mode-on)
-         (c-mode . my-irony-mode-on)
-         (irony-mode . irony-cdb-autosetup-compile-options)))
-
 (use-package flycheck
   :ensure t
   :init
-  (global-flycheck-mode)
-  :config
-  (flycheck-add-next-checker 'c/c++-clang 'c/c++-cppcheck))
-
-(use-package flycheck-yamllint
-  :ensure t
-  :defer t
-  :requires flycheck
-  :hook (flycheck-mode . flycheck-yamllint-setup))
-
-(use-package groovy-mode
-  :ensure t
-  :defer t)
+  (global-flycheck-mode))
 
 (use-package helm
   :ensure t
@@ -165,8 +133,8 @@
                ("C-z"     . helm-select-action))
          (:map helm-command-map
                ("o" . helm-occur)))
+  :functions helm-autoresize-mode
   :config
-  (require 'helm-config)
   (when (executable-find "curl")
     (setq helm-google-suggest-use-curl-p t))
 
@@ -186,55 +154,13 @@
          (:map company-active-map
               ("M-,Av(B" . helm-company))))
 
-(use-package helm-git-grep
-  :ensure t
-  :defer t)
-
-(use-package helm-gtags
-  :when (executable-find "global")
-  :ensure t
-  :defer t
-  :hook ((dired-mode . helm-gtags-mode)
-         (eshell-mode . helm-gtags-mode)
-         (c-mode . helm-gtags-mode)
-         (c++-mode . helm-gtags-mode)
-         (asm-mode . helm-gtags-mode)
-         (objc-mode . helm-gtags-mode))
-  :bind (:map helm-gtags-mode-map
-              ("C-c g a" . 'helm-gtags-tags-in-this-function)
-              ("C-j"     . 'helm-gtags-select)
-              ("M-."     . 'helm-gtags-dwim)
-              ("M-,"     . 'helm-gtags-pop-stack)
-              ("C-c <"   . 'helm-gtags-previous-history)
-              ("C-c >"   . 'helm-gtags-next-history))
-  :config
-  (setq helm-gtags-ignore-case t
-        helm-gtags-auto-update t
-        helm-gtags-use-input-at-cursor t
-        helm-gtags-pulse-at-cursor t
-        helm-gtags-prefix-key "\C-cg"
-        helm-gtags-suggested-key-mapping t))
-
 (use-package helm-projectile
   :ensure t
-  :defer t
-  :after helm-projectile
+  :after (projectile helm)
   :config
   (setq projectile-completion-system 'helm)
-  (helm-projectile-on))
-
-(use-package helm-swoop
-  :ensure t
-  :bind (("M-m" . helm-swoop)
-	 ("M-M" . helm-swoop-back-to-last-point)
-         (:map isearch-mode-map
-               ("M-m" . helm-swoop-from-isearch))
-         (:map helm-swoop-map
-               ("C-r" . helm-previous-line)
-               ("C-s" . helm-next-line))
-         (:map helm-multi-swoop-map
-               ("C-r" . helm-previous-line)
-               ("C-s" . helm-next-line))))
+  (helm-projectile-on)
+  (define-key projectile-command-map (kbd "f") #'helm-projectile-find-file))
 
 (use-package magit
   :ensure t
@@ -247,34 +173,10 @@
   :config
   (projectile-mode))
 
-(use-package robot-mode
-  :load-path "lisp"
-  :commands robot-mode
-  :init
-  (add-to-list 'auto-mode-alist '("\\.robot\\'" . robot-mode)))
-
 ;; Jump between CamelCased words
 (use-package subword
    :init (global-subword-mode t)
    :diminish subword-mode)
-
-(use-package ttcn3
-  :load-path "lisp"
-  :init
-  (add-to-list 'auto-mode-alist '("\\.ttcn\\'" . ttcn-3-mode)))
-
-(use-package yaml-mode
-  :ensure t
-  :defer t)
-
-(use-package yasnippet
-  :ensure t
-  :commands (yas-minor-mode)
-  :hook (prog-mode . yas-minor-mode)
-  :config
-  (use-package yasnippet-snippets
-    :ensure t)
-  (yas-reload-all))
 
 ;;; Load external config
 (when (file-exists-p (emacs-path "extra_init.el"))
--- dotfiles/.emacs.d/settings.el
+++ dotfiles/.emacs.d/settings.el
@@ -1,45 +1,83 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ac-auto-show-menu 2.0)
- '(ac-auto-start 2)
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(backup-directory-alist '(("." . "~/.emacs.d/emacssaves")))
- '(c-basic-offset 4)
- '(column-number-mode t)
- '(create-lockfiles nil)
- '(custom-enabled-themes '(manoj-dark))
- '(custom-file "~/.emacs.d/settings.el")
- '(desktop-load-locked-desktop t)
- '(flycheck-cppcheck-checks
-   '("style" "performance" "portability" "information" "missingInclude"))
- '(global-auto-revert-mode t)
- '(helm-ag-insert-at-point 'symbol)
- '(helm-autoresize-mode t)
- '(helm-git-grep-candidate-number-limit 3000)
- '(indent-tabs-mode nil)
- '(inhibit-startup-screen t)
- '(line-move-visual nil)
- '(list-directory-brief-switches "-aF")
- '(list-directory-verbose-switches "-alsig")
- '(next-screen-context-lines 2)
- '(package-selected-packages
-   (quote
-    (yasnippet diff-hl avy helm-swoop magit helm-company function-args company-irony yasnippet-snippets smartparens diminish highlight-indent-guides company-anaconda anaconda-mode groovy flycheck-clang-tidy projectile flycheck-yamllint flycheck groovy-mode helm-git-grep elpy highlight-chars tdd tide flymake-yaml yaml-mode go-autocomplete helm-core helm ac-helm lua-mode helm-ag neotree helm-go-package go-eldoc exec-path-from-shell helm-projectile helm-gtags go-mode company)))
- '(scroll-conservatively 10000)
- '(scroll-step 1)
- '(tool-bar-mode nil)
- '(visible-bell t)
- '(whitespace-line-column 100)
- '(windmove-wrap-around t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;; settings.el --- Settings file for Emacs -*- lexical-binding: t -*-
+;;; Commentary:
+;; Emacs Settings File --- for adding Emacs settings
+;;; Code:
+
+;; --- Color/Theme Settings ---
+;; ANSI color configuration for terminal output
+(setq ansi-color-faces-vector
+      [default default default italic underline success warning error])
+(setq ansi-color-names-vector
+      ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+
+;; Theme customization
+(setq custom-enabled-themes '(manoj-dark))
+
+;; --- File/Backup Settings ---
+;; Store backup files in a dedicated directory
+(setq backup-directory-alist '(("." . "~/.emacs.d/emacssaves")))
+;; Don't create lock files
+(setq create-lockfiles nil)
+
+;; --- Indentation Settings ---
+;; Use spaces instead of tabs
+(setq indent-tabs-mode nil)
+
+;; --- UI Display Settings ---
+;; Show column numbers in modeline
+(setq column-number-mode t)
+;; Don't show startup screen
+(setq inhibit-startup-screen t)
+;; Use visual line movement
+(setq line-move-visual nil)
+;; Hide toolbar
+(setq tool-bar-mode nil)
+;; Use visible bell instead of beeping
+(setq visible-bell t)
+;; Line length indicator
+(setq whitespace-line-column 100)
+
+;; --- Scrolling Behavior ---
+;; Scroll by one line at a time
+(setq scroll-conservatively 10000)
+(setq scroll-step 1)
+;; Keep context when scrolling to next screen
+(setq next-screen-context-lines 2)
+
+;; --- Session/Desktop Settings ---
+;; Load locked desktop sessions
+(setq desktop-load-locked-desktop t)
+;; Eagerly restore 10 buffers when loading desktop
+(setq desktop-restore-eager 10)
+;; Enable auto-revert mode globally
+(setq global-auto-revert-mode t)
+
+;; --- File Listing Settings ---
+(setq list-directory-brief-switches "-aF")
+(setq list-directory-verbose-switches "-alsig")
+
+;; --- Search/Helm Settings ---
+;; Auto-resize the helm minibuffer
+(setq helm-autoresize-mode t)
+;; Limit git grep candidates for performance
+(setq helm-git-grep-candidate-number-limit 3000)
+
+;; --- Buffer Management ---
+;; Delete duplicate history entries
+(setq history-delete-duplicates t)
+;; Clean old buffers every 14 days
+(setq clean-buffer-list-delay-general 14)
+;; Run midnight mode (periodically clean buffers)
+(setq midnight-mode t)
+
+;; --- Navigation Settings ---
+;; Allow windmove to wrap around edges
+(setq windmove-wrap-around t)
+;; Disable mouse support in xterm
+(setq xterm-mouse-mode nil)
+
+;; --- Face/Appearance Settings ---
+;; Use terminal's background color instead of forcing one
+(set-face-attribute 'default nil :background "unspecified-bg")
+
+;;; settings.el ends here
