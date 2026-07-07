c6acea90a899e6c46436b7a48b8efd8453a975ca
Peter Salin <21469820+psalin@users.noreply.github.com>
2026-07-07 08:38:49 +0200

    scripts: update non-ui setup
    - add script for installing tmux
    - add own script for zoxide
    - update emacs script to support source installation
    - install bat
    - add tarball download function to utils

--- scripts/emacs.sh
+++ scripts/emacs.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #
-# Installs emacs from snap or an AppImage
+# Installs emacs from snap, sources or an AppImage
 #
-# For having access to a recent version of emacs. AppImage
-# is generally needed when sudo access is not available.
-# AppImage is only used when snap installation is not possible.
+# For having access to a recent version of emacs.
 
 dir_path=$(dirname -- "${BASH_SOURCE[0]}")
 source "${dir_path}/utils.inc.sh"
@@ -16,6 +14,13 @@ function get_current_emacs_version() {
     if [[ -x "$(command -v emacs)" ]]; then
         emacs --version | head -n1 | cut -d ' ' -f3
     fi
+}
+
+function query_install() {
+    local -r query_text="$1"
+    read -p "${query_text} (y/n): " -n 1 -r
+    echo
+    [[ $REPLY =~ ^[Yy]$ ]]
 }
 
 function install_using_snap() {
@@ -34,14 +39,66 @@ function install_using_appimage() {
     return $?
 }
 
+function install_from_sources() {
+    local -r github_project="emacs-mirror"
+    local -r github_repo="emacs"
+    local -r tmpdir=$(mktemp -d)
+    local -r current_version=$(get_current_emacs_version)
+    local -r bin_dir="${HOME}"/.local/bin
+    local -r install_dir="${bin_dir}"/emacs-install
+
+    __log_info "Trying to install from sources"
+
+    # shellcheck disable=SC2064
+    trap "trap - RETURN; popd > /dev/null; rm -rf ${tmpdir}" RETURN
+
+    run_cmd echo "Temporary directory: ${tmpdir}"
+    pushd "${tmpdir}" > /dev/null
+
+    if ! package=$(utils::download_github_tag_tarball "${github_project}" "${github_repo}" "${tmpdir}" "${current_version}"); then
+        __log_error "Failed to download emacs"
+        return 1
+    elif [[ -z "${package}" ]]; then
+        return 0
+    fi
+    tar -xzf "${package}"
+
+    # Build emacs
+    __log_info "Building emacs"
+    cd emacs-*/
+    if ! run_cmd ./autogen.sh; then
+        __log_error "Failed to run autogen for emacs"
+        return 1
+    fi
+    if ! run_cmd ./configure --prefix="${install_dir}" --with-x-toolkit=no --with-xpm=ifavailable --with-jpeg=ifavailable --with-gif=ifavailable --with-tiff=ifavailable --with-gnutls=ifavailable; then
+        __log_error "Failed to configure emacs"
+        return 1
+    fi
+    if ! run_cmd make; then
+        __log_error "Failed to make emacs"
+        return 1
+    fi
+
+    __log_info "Install emacs to ${install_dir}"
+    rm -rf "${install_dir}"
+    if ! run_cmd make install; then
+        __log_error "Failed to make install emacs"
+        return 1
+    fi
+
+    # Copy the built binary to .local/bin
+    __log_info "Symlink emacs to ${HOME}/.local/bin"
+    rm -f "${bin_dir}"/emacs
+    ln -s "${install_dir}"/bin/emacs "${bin_dir}"/emacs
+}
+
 function install_emacs() {
     __log_info "Installing/updating Emacs"
     __log_info "Emacs version before: $(get_current_emacs_version)"
 
     install_using_snap && return 0
-    install_using_appimage && return 0
-
-    __log_info "Emacs version after: $(get_current_emacs_version)"
+    install_from_sources && return 0
+    query_install "Install using AppImage" && install_using_appimage && return 0
 }
 
 install_emacs
---
+++ scripts/tmux.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+#
+# Installs tmux from snap, sources or an AppImage
+#
+# For having access to a recent version of tmux
+
+dir_path=$(dirname -- "${BASH_SOURCE[0]}")
+source "${dir_path}/utils.inc.sh"
+set -euo pipefail
+
+TOOLNAME="tmux"
+
+function get_current_tmux_version() {
+    if [[ -x "$(command -v tmux)" ]]; then
+        tmux -V | head -n1 | cut -d ' ' -f2
+    fi
+}
+
+function query_install() {
+    local -r query_text="$1"
+    read -p "${query_text} (y/n): " -n 1 -r
+    echo
+    [[ $REPLY =~ ^[Yy]$ ]]
+}
+
+function install_using_snap() {
+    local -r current_version=$(get_current_tmux_version)
+    utils::install_using_snap "${TOOLNAME}" "${current_version}" "true"
+    return $?
+}
+
+function install_using_appimage() {
+    local -r github_project="kiyoon"
+    local -r github_repo="tmux-appimage"
+    local -r package_type="tmux-appimage"
+    local -r output_dir="${HOME}/.local/bin"
+    local -r current_version=$(get_current_tmux_version)
+    utils::install_github_appimage "${TOOLNAME}" "${github_project}" "${github_repo}" "${package_type}" "${output_dir}" "${current_version}"
+    return $?
+}
+
+function install_from_sources() {
+    local -r ncurses_version="6.5"
+
+    __log_info "Trying to install tmux from sources"
+    tmpdir=$(mktemp -d)
+    run_cmd echo "Temporary directory: ${tmpdir}"
+    pushd "${tmpdir}" > /dev/null
+    local -r build_dir="${tmpdir}/build_dir"
+
+    # shellcheck disable=SC2064
+    trap "trap - RETURN; popd > /dev/null; rm -rf ${tmpdir}" RETURN
+
+    # Download and extract the needed sources
+    __log_info "Downloading tmux"
+    if ! package=$(utils::download_github_release tmux tmux tmux . "$(get_current_tmux_version)"); then
+        __log_error "Failed to download tmux"
+        return 1
+    elif [[ -z "${package}" ]]; then
+        return 0
+    fi
+    __log_info "Downloading libevent"
+    if ! package=$(utils::download_github_release libevent libevent stable .); then
+        __log_error "Failed to download libevent"
+        return 1
+    fi
+    __log_info "Downloading ncurses"
+    if ! run_cmd curl -O https://ftp.gnu.org/gnu/ncurses/ncurses-"${ncurses_version}".tar.gz && \
+	    ! run_cmd curl -O https://mirrors.dotsrc.org/gnu/ncurses/ncurses-"${ncurses_version}".tar.gz; then
+	__log_error "Failed to download ncurses"
+	return 1
+    fi
+    for f in *.tar.gz; do tar -xzf "$f"; done
+
+    # Setup libevent
+    __log_info "Building libevent"
+    cd libevent-*-stable
+    if ! run_cmd ./configure --prefix="${build_dir}" --disable-shared; then
+        __log_error "Failed to configure libevent"
+        return 1
+    fi
+    if ! run_cmd make; then
+        __log_error "Failed to make libevent"
+        return 1
+    fi
+    if ! run_cmd make install; then
+        __log_error "Failed to make install libevent"
+        return 1
+    fi
+    cd ..
+
+    # Setup ncurses
+    __log_info "Building ncurses"
+    cd ncurses-"${ncurses_version}"
+    if ! run_cmd ./configure --prefix="${build_dir}"; then
+        __log_error "Failed to configure ncurses"
+        return 1
+    fi
+    if ! run_cmd make; then
+        __log_error "Failed to make ncurses"
+        return 1
+    fi
+    if ! run_cmd make install; then
+        __log_error "Failed to make install ncurses"
+        return 1
+    fi
+    cd ..
+
+    # Build tmux
+    __log_info "Building tmux"
+    cd tmux-*/
+    if ! run_cmd ./configure CFLAGS="-I${build_dir}/include -I${build_dir}/include/ncurses" LDFLAGS="-L${build_dir}/lib -L${build_dir}/include/ncurses -L${build_dir}/include"; then
+        __log_error "Failed to configure tmux"
+        return 1
+    fi
+    export CPPFLAGS="-I${build_dir}/include -I${build_dir}/include/ncurses"
+    export LDFLAGS="-static -L${build_dir}/include -L${build_dir}/include/ncurses -L${build_dir}/lib"
+    if ! run_cmd make; then
+        __log_error "Failed to make tmux"
+        return 1
+    fi
+
+    # Copy the built binary to .local/bin
+    __log_info "Copying tmux to ${HOME}/.local/bin"
+    if [ -f "${HOME}"/.local/bin/tmux ]; then
+        cp -f "${HOME}"/.local/bin/tmux "${HOME}"/.local/bin/tmux.old
+    fi
+    cp -f tmux "${HOME}"/.local/bin/tmux
+    cd ..
+}
+
+function install_tmux() {
+    __log_info "Installing/updating tmux"
+
+    install_using_snap && return 0
+    install_from_sources && return 0
+    query_install "Install using AppImage" && install_using_appimage && return 0
+}
+
+install_tmux
